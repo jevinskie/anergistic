45262b54e236b729a56cf00304638aa05dacb2af
Jevin Sweval <jevinsweval@gmail.com>
2018-12-16 00:55:28 -0500

    wip

--- mbuf.cpp
+++ mbuf.cpp
@@ -37,28 +37,40 @@ void MemBuf::alloc(u64 ea, u32 sz) {
 	if (is_alloced(ea)) {
 		auto mbuf = find(ea);
 		const auto buf_ea = mbuf.first;
-		auto buf = &mbuf.second.get();
-		const auto buf_sz = (*buf)->size();
+		auto &buf = *mbuf.second.get();
+		const auto buf_sz = buf.size();
 		const auto buf_ea_end = buf_ea + buf_sz;
 		const auto over = buf_ea_end - (ea + sz);
 		if (over > 0) {
-			(*buf)->resize(buf_sz + over);
-			memset((*buf)->data() + buf_sz, 0, over);
+			printf("MemBuf::alloc(0x%016llx, 0x%08x) resize\n", ea, sz);
+			buf.resize(buf_sz + over);
+			memset(buf.data() + buf_sz, 0, over);
 		}
 	} else {
 		mbufs[ea] = std::make_unique<mbufs_t::mapped_type::element_type>(sz, 0);
 	}
 	if (is_alloced(ea + sz)) {
-		printf("found adjacent\n");
+		printf("found adjacent after\n");
 		auto mbuf = find(ea);
-		auto buf = &mbuf.second.get();
-		const auto buf_sz = (*buf)->size();
+		auto &buf = *mbuf.second.get();
+		const auto buf_sz = buf.size();
 		auto adj_mbuf = find(ea + sz);
-		auto adj_buf = &mbuf.second.get();
-		const auto adj_buf_sz = (*adj_buf)->size();
-		(*buf)->resize(buf_sz + adj_buf_sz);
-		memcpy((*buf)->data() + buf_sz, (*adj_buf)->data(), adj_buf_sz);
+		auto &adj_buf = *adj_mbuf.second.get();
+		const auto adj_buf_sz = adj_buf.size();
+		buf.resize(buf_sz + adj_buf_sz);
+		memcpy(buf.data() + buf_sz, adj_buf.data(), adj_buf_sz);
 		mbufs.erase(adj_mbuf.first);
+	} else if (is_alloced(ea - 1)) {
+		printf("found adjacent before\n");
+		auto mbuf = find(ea);
+		auto &buf = *mbuf.second.get();
+		const auto buf_sz = buf.size();
+		auto prev_adj_mbuf = find(ea - 1);
+		auto &prev_adj_buf = *prev_adj_mbuf.second.get();
+		const auto prev_adj_buf_sz = prev_adj_buf.size();
+		prev_adj_buf.resize(prev_adj_buf_sz + buf_sz);
+		memcpy(prev_adj_buf.data() + prev_adj_buf_sz, buf.data(), buf_sz);
+		mbufs.erase(mbuf.first);
 	}
 }
 
@@ -68,18 +80,15 @@ bool MemBuf::is_alloced(u64 ea) {
 
 bool MemBuf::is_alloced(u64 ea, u32 sz) {
 	printf("MemBuf::is_alloced(0x%016llx, 0x%08x)\n", ea, sz);
-	const auto bufp = mbufs.lower_bound(ea);
-	if (bufp == mbufs.cend()) {
-		printf("MemBuf::is_alloced(0x%016llx, 0x%08x) = false cend\n", ea, sz);
-		return false;
-	}
-	const auto buf_ea = bufp->first;
-	const auto buf = &bufp->second;
-	const auto buf_ea_end = buf_ea + (*buf)->size();
-	printf("MemBuf::is_alloced(0x%016llx, 0x%08x) buf_ea = 0x%016llx buf_ea_end = 0x%016llx\n", ea, sz, buf_ea, buf_ea_end);
-	if (ea >= buf_ea && ea + sz <= buf_ea_end) {
-		printf("MemBuf::is_alloced(0x%016llx, 0x%08x) = true\n", ea, sz);
-		return true;
+	for (const auto &bufp : mbufs) {
+		const auto buf_ea = bufp.first;
+		const auto &buf = *bufp.second;
+		const auto buf_ea_end = buf_ea + buf.size();
+		printf("MemBuf::is_alloced(0x%016llx, 0x%08x) buf_ea = 0x%016llx buf_ea_end = 0x%016llx\n", ea, sz, buf_ea, buf_ea_end);
+		if (ea >= buf_ea && ea + sz <= buf_ea_end) {
+			printf("MemBuf::is_alloced(0x%016llx, 0x%08x) = true\n", ea, sz);
+			return true;
+		}
 	}
 	printf("MemBuf::is_alloced(0x%016llx, 0x%08x) = false\n", ea, sz);
 	return false;
@@ -90,15 +99,16 @@ mbuf_pair_ref_t MemBuf::find(u64 ea) {
 }
 
 mbuf_pair_ref_t MemBuf::find(u64 ea, u32 sz) {
-	auto bufp = mbufs.lower_bound(ea);
-	if (bufp == mbufs.cend()) {
-		assert(!"value does not exist");
-	}
-	const auto buf_ea = bufp->first;
-	auto buf = &bufp->second;
-	const auto buf_ea_end = buf_ea + (*buf)->size();
-	if (ea >= buf_ea && ea + sz <= buf_ea_end) {
-		return std::make_pair(buf_ea, std::ref(bufp->second));
+	printf("MemBuf::find(0x%016llx, 0x%08x)\n", ea, sz);
+	for (auto &bufp : mbufs) {
+		const auto buf_ea = bufp.first;
+		const auto &buf = *bufp.second;
+		const auto buf_ea_end = buf_ea + buf.size();
+		printf("MemBuf::find(0x%016llx, 0x%08x) buf_ea = 0x%016llx buf_ea_end = 0x%016llx\n", ea, sz, buf_ea, buf_ea_end);
+		if (ea >= buf_ea && ea + sz <= buf_ea_end) {
+			printf("MemBuf::find(0x%016llx, 0x%08x) = true\n", ea, sz);
+			return std::make_pair(buf_ea, std::ref(bufp.second));
+		}
 	}
 	assert(!"value does not exist");
 }
